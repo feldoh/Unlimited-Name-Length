7e411fc0a94528e29e547500fefdb7684c765fb1
Dexter Lowe <feldoh10@gmail.com>
2024-02-13 16:28:58 +0000

    POC

--- 1.4/Source/Unlimited_Name_Length/Unlimited_Name_LengthMod.cs
+++ 1.4/Source/Unlimited_Name_Length/Unlimited_Name_LengthMod.cs
@@ -1,19 +1,70 @@
-﻿using Verse;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using HarmonyLib;
+using RimWorld;
+using Verse;
 
 namespace Unlimited_Name_Length;
 
-public class Unlimited_Name_LengthMod : Verse.Mod
+public class Unlimited_Name_LengthMod : Mod
 {
-
     public Unlimited_Name_LengthMod(ModContentPack content) : base(content)
     {
 #if DEBUG
         Harmony.DEBUG = true;
 #endif
 
-        Harmony harmony = new Harmony("Feldoh.rimworld.Unlimited_Name_Length.main");	
+        Harmony harmony = new Harmony("Feldoh.rimworld.Unlimited_Name_Length.main");
         harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(CharacterCardUtility), "DoNameInputRect")]
+public static class CharacterCardUtilityLengthPatch
+{
+    [HarmonyPrefix]
+    public static bool DoNameInputRect(ref int maxLength)
+    {
+        maxLength = int.MaxValue;
+        return true;
+    }
+}
+
+[HarmonyPatch]
+public static class GiveNameLengthPatch
+{
+    [HarmonyTargetMethods]
+    public static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.PropertyGetter(typeof(Dialog_NamePlayerFaction), "FirstCharLimit");
+        yield return AccessTools.PropertyGetter(typeof(Dialog_NamePlayerSettlement), "FirstCharLimit");
+        yield return AccessTools.PropertyGetter(typeof(Dialog_NamePlayerFactionAndSettlement), "FirstCharLimit");
+        yield return AccessTools.PropertyGetter(typeof(Dialog_NamePlayerFactionAndSettlement), "SecondCharLimit");
+        yield return AccessTools.PropertyGetter(typeof(Dialog_GiveName), "FirstCharLimit");
+        yield return AccessTools.PropertyGetter(typeof(Dialog_GiveName), "SecondCharLimit");
+        yield return AccessTools.PropertyGetter(typeof(Dialog_Rename), "MaxNameLength");
+    }
+
+    [HarmonyPostfix]
+    public static void MaxNameLength(ref int __result)
+    {
+        __result = int.MaxValue;
+    }
+}
+
+[HarmonyPatch]
+public static class ValidityPatch
+{
+    [HarmonyTargetMethods]
+    public static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.Method(typeof(NamePlayerFactionDialogUtility), nameof(NamePlayerFactionDialogUtility.IsValidName));
+        yield return AccessTools.Method(typeof(NamePlayerSettlementDialogUtility), nameof(NamePlayerSettlementDialogUtility.IsValidName));
+    }
+
+    [HarmonyPostfix]
+    public static void IsValidName(ref bool __result, string s)
+    {
+        __result = s.Length > 0;
     }
 }
--- README.md
+++ README.md
@@ -6,9 +6,13 @@
 
 # Please Remember to replace the YOUR_WORKSHOP_ITEM_ID placeholder once you upload your mod and have an id and remove this line.
 
-# Unlimited_Name_Length
+# Unlimited Name Length
 
-Unlimited Pawn Name Length
+Unlimited Pawn, faction and settlement name length.
+All restrictions on names are removed so that means it's up to you to not make a name that Rimworld can't handle.
+If you make a name so long it looks silly and doesn't fit in the UI, that's on you, I'm not trying to make it so the full works of Shakespeare can fit in a pawn name and still be in some way sensible.
+
+Nb: Technically there is still a limit of 2147483647 or two billion one hundred forty-seven million four hundred eighty-three thousand six hundred forty-seven characters, but I think that's enough for most people.
 
 ## Disclaimer
 Portions of the materials used to create this content/mod are trademarks and/or copyrighted works of Ludeon Studios Inc. All rights reserved by Ludeon. This content/mod is not official and is not endorsed by Ludeon.
