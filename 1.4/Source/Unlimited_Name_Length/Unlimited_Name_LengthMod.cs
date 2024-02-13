069ad7573c29754e44554ecfbf5f2b728ae4a73a
Dexter Lowe <feldoh10@gmail.com>
2024-02-13 12:24:07 +0000

    Cookie cutter initial commit

---
+++ 1.4/Source/Unlimited_Name_Length/Unlimited_Name_LengthMod.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Unlimited_Name_Length;
+
+public class Unlimited_Name_LengthMod : Verse.Mod
+{
+
+    public Unlimited_Name_LengthMod(ModContentPack content) : base(content)
+    {
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+
+        Harmony harmony = new Harmony("Feldoh.rimworld.Unlimited_Name_Length.main");	
+        harmony.PatchAll();
+    }
+}
